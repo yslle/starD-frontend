906139d123bac790973c11fd1174c0f306b92cc9
dev_me <gjalsdud1030@naver.com>
2023-11-20 02:45:49 +0900

    add: "add findMyLocation function in map"

--- src/components/map/MapNaverDefault.js
+++ src/components/map/MapNaverDefault.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useRef, useState,useCallback} from 'react';
 import axios from "axios";
 
 const MapNaverDefault = ({studyId, Member}) => {
@@ -6,13 +6,59 @@ const MapNaverDefault = ({studyId, Member}) => {
     const accessToken = localStorage.getItem('accessToken');
 
     const [mapLat, setMapLat] = useState(null); // 위도
-    const [mapLng, setMapLng] = useState(null); // 위도
+    const [mapLng, setMapLng] = useState(null); // 경도
     const [currentLocation, setCurrentLocation] = useState({});
     const mapElement = useRef(null);
     const {naver} = window;
-
     const [inputs, setInputs] = useState(["", ""]);
+    const searchResultsRef = useRef(null); //검색결과창
+    const [myLocation, setMyLocation] = useState({}); //현재 위치
 
+    //현재 나의 위치 찾기
+    const MyLocationpoint =useCallback(() => {
+        var options = {
+            enableHighAccuracy: true, //정확도 높이기 -> 완전 정확하지 않음
+            maximumAge: 30000,
+            timeout: 15000
+        }
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error,options);
+        }
+        if (!mapElement.current || !naver) return;
+        const location = new naver.maps.LatLng(myLocation.latitude, myLocation.longitude);
+
+        const mapOptions = {
+            center: location,
+            zoom: 17,
+            zoomControl: true,
+        };
+
+        const map = new naver.maps.Map(mapElement.current, mapOptions);
+        new naver.maps.Marker({
+            position: location,
+            map,
+        });
+        // 위치추적에 성공했을때 위치 값을 넣어줌
+        function success(position) {
+            console.log("성공함?");
+            console.log("위도",position.coords.latitude );
+            console.log("경도",position.coords.longitude );
+            setMyLocation({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+            });
+        }
+
+        // 위치 추적에 실패 했을때 초기값을 넣어줌
+        function error() {
+            console.log("error남")
+            setMyLocation({latitude: 37.4979517, longitude: 127.0276188});
+        }
+
+    },[mapElement,myLocation]);
+
+    //회원가입 시 입력한 구, 군 위치찍기
     useEffect(() => {
         axios.get(`http://localhost:8080/location/${studyId}/all`, {
             withCredentials: true,
@@ -32,6 +78,9 @@ const MapNaverDefault = ({studyId, Member}) => {
 
     }, []);
 
+
+
+
     useEffect(() => {
         if (!mapElement.current || !naver) return;
 
@@ -50,6 +99,33 @@ const MapNaverDefault = ({studyId, Member}) => {
             });
         }
     }, [mapLat, mapLng, naver]);
+
+
+    // 상태가 지리적 위치 정보로 업데이트된 후 지도와 마커 생성처리
+    useEffect(() => {
+        if (!mapElement.current || !naver || !myLocation.latitude || !myLocation.longitude) return;
+
+        const location = new naver.maps.LatLng(myLocation.latitude, myLocation.longitude);
+        const mapOptions = {
+            center: location,
+            zoom: 17,
+            zoomControl: true,
+        };
+
+        const map = new naver.maps.Map(mapElement.current, mapOptions);
+
+        new naver.maps.Marker({
+            position: location,
+            map,
+            // 원하는 이미지로 마커 커스텀
+            // icon: {
+            //     url: pinImage,
+            //     size: new naver.maps.Size(50, 52),
+            //     origin: new naver.maps.Point(0, 0),
+            //     anchor: new naver.maps.Point(25, 26),
+            //   },
+        });
+    }, [mapElement, naver, myLocation]);
 
     const addInput = () => {
         if (inputs.length === Member.length) {
@@ -73,6 +149,7 @@ const MapNaverDefault = ({studyId, Member}) => {
         setInputs(newInputs);
     };
 
+    //중간위치 찾아주기
     const findMidpoint = () => {
         if (inputs.some(input => !input.trim())) {
             alert('모든 입력 값을 올바르게 작성하세요.');
@@ -111,7 +188,11 @@ const MapNaverDefault = ({studyId, Member}) => {
                     )}
                 </div>
             ))}
+            <div ref={searchResultsRef}></div>
+            <div className={"find_btn"}>
             <button onClick={findMidpoint}>찾기</button>
+            <button onClick={MyLocationpoint}>현재 나의 위치</button>
+            </div>
             <div ref={mapElement} style={{height: '250px', width: '580px'}} id={"naver-map"}/>
         </div>
     );
