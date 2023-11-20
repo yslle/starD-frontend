d5d0253e04c43f6cee12c286b6593db477c7b2a6
dev_me <gjalsdud1030@naver.com>
2023-11-20 22:04:43 +0900

    fix: "fix findMyLocation function in map"

--- src/components/map/MapNaverDefault.js
+++ src/components/map/MapNaverDefault.js
@@ -1,5 +1,6 @@
-import React, {useEffect, useRef, useState,useCallback} from 'react';
+import React, {useEffect, useRef, useState, useCallback} from 'react';
 import axios from "axios";
+import { Naver_ID, Naver_Secret } from '../../config';
 
 const MapNaverDefault = ({studyId, Member}) => {
 
@@ -14,8 +15,9 @@ const MapNaverDefault = ({studyId, Member}) => {
     const searchResultsRef = useRef(null); //검색결과창
     const [myLocation, setMyLocation] = useState({}); //현재 위치
 
+
     //현재 나의 위치 찾기
-    const MyLocationpoint =useCallback(() => {
+    const MyLocationpoint = useCallback(() => {
         var options = {
             enableHighAccuracy: true, //정확도 높이기 -> 완전 정확하지 않음
             maximumAge: 30000,
@@ -23,7 +25,7 @@ const MapNaverDefault = ({studyId, Member}) => {
         }
 
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success, error,options);
+            navigator.geolocation.getCurrentPosition(success, error, options);
         }
         if (!mapElement.current || !naver) return;
         const location = new naver.maps.LatLng(myLocation.latitude, myLocation.longitude);
@@ -39,15 +41,48 @@ const MapNaverDefault = ({studyId, Member}) => {
             position: location,
             map,
         });
+
         // 위치추적에 성공했을때 위치 값을 넣어줌
         function success(position) {
             console.log("성공함?");
-            console.log("위도",position.coords.latitude );
-            console.log("경도",position.coords.longitude );
+            console.log("위도", position.coords.latitude);
+            console.log("경도", position.coords.longitude);
             setMyLocation({
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude,
             });
+            const location = new naver.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            const reverseGeocodeUrl = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${position.coords.longitude},${position.coords.latitude}&orders=addr`;
+
+            axios.get(reverseGeocodeUrl, {
+                headers: {
+                    'X-NCP-APIGW-API-KEY-ID': Naver_ID,
+                    'X-NCP-APIGW-API-KEY': Naver_Secret
+                },
+            })
+                .then(response => {
+                    // Extract the address from the response
+                    const address = response.data.addresses[0].addrDetail;
+                    console.log('주소:', address);
+
+                    // Create the map with the obtained coordinates
+                    const mapOptions = {
+                        center: location,
+                        zoom: 17,
+                        zoomControl: true,
+                    };
+
+                    const map = new naver.maps.Map(mapElement.current, mapOptions);
+
+                    // Add a marker to the map
+                    new naver.maps.Marker({
+                        position: location,
+                        map,
+                    });
+                })
+                .catch(error => console.error('Error in reverse geocoding:', error));
+
         }
 
         // 위치 추적에 실패 했을때 초기값을 넣어줌
@@ -56,9 +91,11 @@ const MapNaverDefault = ({studyId, Member}) => {
             setMyLocation({latitude: 37.4979517, longitude: 127.0276188});
         }
 
-    },[mapElement,myLocation]);
+        return myLocation;
 
-    //회원가입 시 입력한 구, 군 위치찍기
+    }, [mapElement, myLocation]);
+
+    //회원가입 시 입력한 구, 군 위치찍기 -> 위도 경도 초기화
     useEffect(() => {
         axios.get(`http://localhost:8080/location/${studyId}/all`, {
             withCredentials: true,
@@ -80,12 +117,12 @@ const MapNaverDefault = ({studyId, Member}) => {
 
 
 
-
     useEffect(() => {
         if (!mapElement.current || !naver) return;
 
         if (naver.maps) {
             const location = new naver.maps.LatLng(mapLng, mapLat);
+            console.log("위치:", location);
             const mapOptions = {
                 center: location,
                 zoom: 16,
@@ -97,9 +134,9 @@ const MapNaverDefault = ({studyId, Member}) => {
                 position: location,
                 map,
             });
+
         }
     }, [mapLat, mapLng, naver]);
-
 
     // 상태가 지리적 위치 정보로 업데이트된 후 지도와 마커 생성처리
     useEffect(() => {
@@ -144,10 +181,12 @@ const MapNaverDefault = ({studyId, Member}) => {
     };
 
     const handleInputChange = (index, value) => {
+        console.log(value);
         const newInputs = [...inputs];
         newInputs[index] = value;
         setInputs(newInputs);
-    };
+    }
+
 
     //중간위치 찾아주기
     const findMidpoint = () => {
@@ -177,21 +216,31 @@ const MapNaverDefault = ({studyId, Member}) => {
     return (
         <div className={"map_content"}>
             {inputs.map((input, index) => (
-                <div key={index} id={"input-location"}>
-                    <input placeholder={" 장소를 입력하세요."}
-                           onChange={(e) => handleInputChange(index, e.target.value)}/>
-                    {inputs.length - 1 === index && (
-                        <button onClick={addInput}>+</button>
-                    )}
-                    {inputs.length !== 1 && index > 1 && (
-                        <button onClick={() => removeInput(index)}>-</button>
-                    )}
+                <div>
+                    <div key={index} id={"input-location"}>
+                        <input placeholder={" 장소를 입력하세요."}
+                               onChange={(e) => handleInputChange(index, e.target.value)}/>
+                        {inputs.length - 1 === index && (
+                            <button onClick={addInput}>+</button>
+                        )}
+                        {inputs.length !== 1 && index > 1 && (
+                            <button onClick={() => removeInput(index)}>-</button>
+                        )}
+                    </div>
+                    {/*<div id="searchResults">*/}
+                    {/*    {items.map((item, index) => (*/}
+                    {/*        <div key={index} className="result-item" onClick={() => handleResultItemClick(index)}>*/}
+                    {/*            {item.jibunAddress}*/}
+                    {/*        </div>*/}
+                    {/*    ))}*/}
+                    {/*</div>*/}
                 </div>
             ))}
+
             <div ref={searchResultsRef}></div>
             <div className={"find_btn"}>
-            <button onClick={findMidpoint}>찾기</button>
-            <button onClick={MyLocationpoint}>현재 나의 위치</button>
+                <button onClick={findMidpoint}>찾기</button>
+                <button onClick={MyLocationpoint}>현재 나의 위치</button>
             </div>
             <div ref={mapElement} style={{height: '250px', width: '580px'}} id={"naver-map"}/>
         </div>
