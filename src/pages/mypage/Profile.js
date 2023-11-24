18e8031a4784d641b1f147f778199f7b12b12aa8
dev_me <gjalsdud1030@naver.com>
2023-11-24 23:25:32 +0900

    add: "프로필 사진 업로드 및 삭제 구현"

--- src/pages/mypage/Profile.js
+++ src/pages/mypage/Profile.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Category from "../../components/repeat_etc/Category";
 import Backarrow from "../../components/repeat_etc/Backarrow"
 import Header from "../../components/repeat_etc/Header";
+import default_profile_img from "../../images/default_profile_img.png";
 
-const Profile=()=>{
+const Profile = () => {
+    const [uploadImgUrl, setUploadImgUrl] = useState(null);
 
-    return(
+    //프로필 사진 업로드
+    const onchangeImageUpload = (e) => {
+        console.log("사진", e.target.files);
+        const file = e.target.files[0];
+        const imageUrl = URL.createObjectURL(file);
+        setUploadImgUrl(imageUrl);
+    }
+
+    //프로필 사진 삭제
+    const onchangeImageDelete = (e) => {
+        setUploadImgUrl(null);
+    }
+
+    return (
         <div>
             <Header showSideCenter={true}/>
             <div className="container">
@@ -14,10 +29,18 @@ const Profile=()=>{
                     <p id={"entry-path"}> 홈 > 마이페이지 > 프로필 </p>
                     <Backarrow subname={"프로필"}/>
                     <div className="sub_container">
+                        {uploadImgUrl ? (
+                            <img className="profile-img" src={uploadImgUrl} alt="프로필 없을때"/>
+                        ) : (
+
+                            <img className="profile-img" src={default_profile_img} alt="프로필사진"/>
+                        )}
+                        <input className="image-upload" type="file" accept="image/*" onChange={onchangeImageUpload}/>
+                        <button onClick={onchangeImageDelete}> 프로필 사진 삭제하기</button>
                     </div>
                 </div>
             </div>
         </div>
     )
-}
-export default  Profile;
+};
+export default Profile;
