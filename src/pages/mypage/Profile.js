1c3e2bc61e3dc7591fa67f2438829cad457cd401
dev_me <gjalsdud1030@naver.com>
2023-11-25 00:06:28 +0900

    add: "한 줄 소개 추가"

--- src/pages/mypage/Profile.js
+++ src/pages/mypage/Profile.js
@@ -6,18 +6,30 @@ import default_profile_img from "../../images/default_profile_img.png";
 
 const Profile = () => {
     const [uploadImgUrl, setUploadImgUrl] = useState(null);
-
+    const [selfintro, setSelfIntro] = useState("");
     //프로필 사진 업로드
     const onchangeImageUpload = (e) => {
         console.log("사진", e.target.files);
         const file = e.target.files[0];
-        const imageUrl = URL.createObjectURL(file);
-        setUploadImgUrl(imageUrl);
+        if (file) {
+            console.log("File details:", file);
+            const imageUrl = URL.createObjectURL(file);
+            setUploadImgUrl(imageUrl);
+        } else {
+            console.error("No file selected");
+            alert("이미지를 선택해주세요");
+            return;
+        }
     }
 
     //프로필 사진 삭제
     const onchangeImageDelete = (e) => {
         setUploadImgUrl(null);
+        return;
+    }
+
+    const onchangeselfIntroduce=(e)=>{
+        setSelfIntro(e.target.value);
     }
 
     return (
@@ -29,14 +41,25 @@ const Profile = () => {
                     <p id={"entry-path"}> 홈 > 마이페이지 > 프로필 </p>
                     <Backarrow subname={"프로필"}/>
                     <div className="sub_container">
-                        {uploadImgUrl ? (
-                            <img className="profile-img" src={uploadImgUrl} alt="프로필 없을때"/>
-                        ) : (
+                        <div className={"profile_content"}>
+                            {uploadImgUrl ? (
+                                <img className="profile-img" src={uploadImgUrl} alt="프로필 없을때"/>
+                            ) : (
 
-                            <img className="profile-img" src={default_profile_img} alt="프로필사진"/>
-                        )}
-                        <input className="image-upload" type="file" accept="image/*" onChange={onchangeImageUpload}/>
-                        <button onClick={onchangeImageDelete}> 프로필 사진 삭제하기</button>
+                                <img className="profile-img" src={default_profile_img} alt="프로필사진"/>
+                            )}
+                            <input className="image-upload" type="file" accept="image/*"
+                                   onChange={onchangeImageUpload}/>
+                            <button className="image-delete" onClick={onchangeImageDelete}>삭제</button>
+                        </div>
+                        <div className={"One-line-self-introduction"}>
+                            <p id={"self-intro-p"}>한줄 자기소개</p>
+                            <input placeholder={"15자이내 자기소개를 적어주세요"} onChange={onchangeselfIntroduce}/>
+
+                        </div>
+                    </div>
+                    <div className={"save_profile_content"}>
+                        <button className={"save-profile"}>저장하기</button>
                     </div>
                 </div>
             </div>
